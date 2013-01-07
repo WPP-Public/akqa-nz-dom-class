891f9ed3954d9b88ab075b795fca0e918a215a2f
Pieter Vanderwerff <pieter@heyday.co.nz>
2013-01-07 16:03:56 +1300

    UPDATE: added JSDoc comments, updated AMD wrapper, cleaned up Class.prototype function definitions

--- class.js
+++ class.js
@@ -1,48 +1,95 @@
+/** @license MIT License (c) copyright Heyday Digital */
 
-// AMD or browser wrapper for module format
-( function( root, factory ) {
-	if ( typeof define === 'function' && define.amd ) {
-		define( factory );
-	} else {
-		root.DomClass = factory();
-	}
-}( this, function() {
-	'use strict';
+/**
+ * An extremely miminal DOM element class manipulator
+ *
+ * Licensed under the MIT License at:
+ * http://heyday.mit-license.org/
+ *
+ * @version 0.1.0
+ */
 
-	var Class, wrap, trim, empty = ' ';
+(function(define) { 'use strict';
+define( function() {
 
-	// Helper functions
+	var Class, wrap, trim, EMPTY = ' ';
+
+	/**
+	 * Wrap string in whitespace
+	 * @private
+	 * @param  {String} str String to be wrapped
+	 * @return {String}
+	 */
 	wrap = function( str ) {
-		return empty + str + empty;
+		return EMPTY + str + EMPTY;
 	};
+
+	/**
+	 * Trim additional white space off the start and end of a string
+	 * @private
+	 * @param  {String} str String to be trimmed
+	 * @return {String}
+	 */
 	trim = function( str ) {
 		return !str ? '' : str.toString().replace( /^\s+/, '' ).replace( /\s+$/, '' );
 	};
 
-	// Constructor
+
+	/**
+	 * Constructor for the dom-class manipulator
+	 * @param {DOMNode} el Dom node to proform class manpulations on
+	 * @constructor
+	 */
 	Class = function( el ) {
 		this._el = el;
 	};
 
-	Class.has = function( str ) {
-		return !!~( this.get() ).indexOf( wrap( str ) );
-	};
+	Class.prototype = {
 
-	Class.get = function() {
-		return wrap( this._el.className );
-	};
+		/**
+		 * Check if DOMNode has a specific Class
+		 * @param  {String}  str String to be searched for in Class list
+		 * @return {Boolean}
+		 */
+		has: function( str ) {
+			return !!~( this.get() ).indexOf( wrap( str ) );
+		},
 
-	Class.add = function( str ) {
-		if ( !this.has( str ) ) {
-			this._el.className += empty + str;
+		/**
+		 * Get Class names for DOMNode
+		 * @return {String}
+		 */
+		get: function() {
+			return wrap( this._el.className );
+		},
+
+		/**
+		 * Add Class to DOMNode Class list if not already added
+		 * @param {String} str Class to be added
+		 * @return {Class} Referance to itself, for chaining
+		 */
+		add: function( str ) {
+			if ( !this.has( str ) ) {
+				this._el.className += EMPTY + str;
+			}
+			return this;
+		},
+
+		/**
+		 * Remove Class from DOMNode Class list
+		 * @param  {String} str String to be removed
+		 * @return {Class} Referance to itself, for chaining
+		 */
+		remove: function( str ) {
+			this._el.className = trim( ( this.get() ).replace( wrap( str ), EMPTY ) );
+			return this;
 		}
-		return this;
-	};
-
-	Class.remove = function( str ) {
-		this._el.className = trim( ( this.get() ).replace( wrap( str ), empty ) );
-		return this;
 	};
 
 	return Class;
-} ) );
+} );
+} )( typeof define == 'function'
+	? define
+	: function( deps, factory ) { this.DomClass = factory(); }
+	// Boilerplate for AMD, and browser global
+);
